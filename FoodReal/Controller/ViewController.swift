e0a3f60cc10c88c34c0c103a2e0716ebd25fd817
Yunseo Claire Han <yunseohan01@gmail.com>
2023-02-03 00:10:06 -0800

    Finished setting up Firebase and Firestore.

--- FoodReal/Controller/ViewController.swift
+++ FoodReal/Controller/ViewController.swift
@@ -6,11 +6,28 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Add a new document with a generated ID
+        let db = Firestore.firestore()
+        var ref: DocumentReference? = nil
+        ref = db.collection("users").addDocument(data: [
+            "first": "Ada",
+            "last": "Lovelace",
+            "born": 1815
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
         
         print("Hello")
     }

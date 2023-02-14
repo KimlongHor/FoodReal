8da9844c4a8cd07d80eac667f4c27055fe3df924
Kimlong Hor <kimlong.hor@sjsu.edu>
2023-02-14 09:57:20 -0800

    Add PostCollectionViewCell

--- dev/FoodReal/Helpers/UIViewExtensions.swift
+++ dev/FoodReal/Helpers/UIViewExtensions.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIView {
+    func createRoundCorner(cornerRadius: CGFloat) {
+        self.layer.cornerRadius = cornerRadius
+    }
+
+    func createRoundCornerForLabel(cornerRadius: CGFloat){
+        self.layer.cornerRadius = cornerRadius
+        self.layer.masksToBounds = true
+    }
+    
+    func createSpecificRoundCorner(at mask: CACornerMask ,radius: CGFloat) {
+        self.layer.maskedCorners = mask
+        self.layer.cornerRadius = radius
+        self.layer.masksToBounds = true
+    }
+    
+    func createBorder(color: UIColor, width: CGFloat) {
+       self.layer.borderColor = color.cgColor
+       self.layer.borderWidth = width
+    }
+}

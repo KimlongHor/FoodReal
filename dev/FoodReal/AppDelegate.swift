69fc4da2844e9953bf43ca22cbc90f26b32f9f44
Kimlong Hor <kimlong.hor@sjsu.edu>
2023-03-23 10:11:04 -0700

    Add local notification

--- dev/FoodReal/AppDelegate.swift
+++ dev/FoodReal/AppDelegate.swift
@@ -8,12 +8,19 @@
 import UIKit
 import FirebaseCore
 import FirebaseFirestore
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("User gave permission for local notificaiton")
+            }
+        }
         
         FirebaseApp.configure()
         return true
--- dev/FoodReal/SceneDelegate.swift
+++ dev/FoodReal/SceneDelegate.swift
@@ -19,13 +19,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        if (Auth.auth().currentUser==nil) {
-            let onboardingNameViewController = storyboard.instantiateViewController(withIdentifier: "OnboardingNameViewController")
-            window?.rootViewController = onboardingNameViewController
-        } else {
+//        if (Auth.auth().currentUser==nil) {
+//            let onboardingNameViewController = storyboard.instantiateViewController(withIdentifier: "OnboardingNameViewController")
+//            window?.rootViewController = onboardingNameViewController
+//        } else {
             let mainNavigationController = storyboard.instantiateViewController(withIdentifier: "MainNavigationController")
             window?.rootViewController = mainNavigationController
-        }
+//        }
     }
     
     func changeRootViewController(_ vc: UIViewController, animated: Bool = true) {
@@ -45,6 +45,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        UIApplication.shared.applicationIconBadgeNumber = 0
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
